149841b02edb6a1736ca40315b95f55ec14d1590
Menna Bashir <150310423+MennaBashir@users.noreply.github.com>
2025-03-26 13:18:44 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/Portfolio/",
+  base: "/Portfolio/",
 });
-// "homepage": "https://mennabashir.github.io/Portfolio", if add add in package.jscon
